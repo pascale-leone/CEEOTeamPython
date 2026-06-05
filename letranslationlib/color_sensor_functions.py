9571bb7e88dce59320b3cea105429af7dd20da28
Maggie Anthes <maggieanthes26@gmail.com>
2026-06-05 16:28:50 -0400

    added number to color mapping

--- letranslationlib/color_sensor_functions.py
+++ letranslationlib/color_sensor_functions.py
@@ -6,18 +6,35 @@ class colorSensor(le.ColorSensor):
         super().__init__()
     
     def detect_color(self):
-        detected_color = self.sensor.color
-        return detected_color
+        color_number = self.sensor.color
+        color_mapping = {
+            0: 'No color',
+            1: 'Red',
+            2: 'Yellow',
+            3: 'Blue',
+            4: 'Teal',
+            5: 'Green',
+            6: 'Purple',
+            7: 'White',
+            8: 'Magenta',
+            9: 'Orange',
+            10: 'Azure'
+        }
         #detect the color, return the detected color
-
+        return color_mapping.get(color_number, 'Unknown')
+    
 card_color = le.LEGO_COLOR_PURPLE
 card_serial = '1131'
 
 cs = colorSensor()
 
 cs.connect(card_color=card_color, card_serial=card_serial)
+if not cs.connected:
+    print('Error connecting to Color Sensor.')
+    exit(1) # error connecting
 
-for i in range(10):
+time.sleep(5)
+for i in range(21):
     color = cs.detect_color()
     print(f'Detected color: {color}')
     time.sleep(5)
