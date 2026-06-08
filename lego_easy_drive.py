9100dbc2cda1c092aa3e6340ed3f342a780be196
pascale-leone <pleone@oberlin.edu>
2026-06-08 10:56:26 -0400

    testing drive

---
+++ lego_easy_drive.py
@@ -0,0 +1,12 @@
+from lego_easy import Motor, Robot, ColorSensor, Controller, wait
+from lego_easy import COLOR_RED, COLOR_GREEN, COLOR_BLUE
+
+MY_CARD = "1128"
+
+robot = Robot(MY_CARD, speed=100)
+controller = Controller(MY_CARD)
+
+for i in range(200):
+    controller.drive_robot(robot)
+    wait(0.1)
+robot.stop()
