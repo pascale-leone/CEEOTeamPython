597dd23aaddcacbbd38bd3179fdb3e7b18876abf
pascale-leone <pleone@oberlin.edu>
2026-06-11 10:04:08 -0400

    changed name

--- letranslationlib/main.js
+++ letranslationlib/main.js
@@ -29,7 +29,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 700,
-    title: 'Easy Lego',
+    title: 'EasyLego',
     webPreferences: { nodeIntegration: false }
   });
 
